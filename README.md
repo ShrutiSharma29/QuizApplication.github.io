f0c230193bf2fd1accb19bdf16a4ec23f3f60573
shrutis2908@gmail.com <shrutis2908@gmail.com>
2023-07-17 00:24:38 +0530

    QuizApplication

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # QuizApplication.github.io
+
+
+The Quiz Application is an interactive web-based platform that allows users to participate in quizzes and test their knowledge on various subjects. The application has been developed using a combination of CSS, HTML, and JavaScript to create an engaging and user-friendly experience. 
+
+key features : 
+
+Timer and Scoring: Each quiz is timed, and users earn points based on their accuracy and speed.
+
+Multiple Question Types: The application supports different question formats, including multiple choice, true/false, and open-ended questions.
+
+Instant Feedback: Users receive immediate feedback after submitting each question, including the correct answer and their score.
+
+Progress Tracking: The application keeps track of users' quiz history, displaying their scores and performance statistics.
---
+++ js/questions.js
@@ -0,0 +1,59 @@
+//  array for  number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "which of the following is not a CSS selector which is used to find the HTML element we want to style ?",
+    answer: "Complex Selectors",
+    options: [
+      "Complex Selectors",
+      "Combinator Selectors",
+      "Pseudo-element Selectors",
+      "Simple Selectors"
+    ]
+    
+  },
+    {
+    numb: 2,
+    question: "Which od the following is not a built-in module in Node.js?",
+    answer: "mysql",
+    options: [
+      "fs",
+      "http",
+      "mysql",
+      "path"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following statement is True about PHP?",
+    answer: "PHP is a server side scripting language",
+    options: [
+      "PHP is a server side scripting language",
+      "PHP is a client side scripting language",
+      "PHP is a database",
+      "None of above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
